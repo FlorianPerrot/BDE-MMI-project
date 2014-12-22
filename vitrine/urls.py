ee854e4b8035e28b98fc578b3f73fd25d6b99dca
Florian Perrot <contact@florian-perrot.fr>
2014-12-22 17:16:05 +0100

    Root vers le controleur "gallerie" puis vers une view "gallerie_mobile".

--- vitrine/urls.py
+++ vitrine/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('vitrine.views',
     url(r'^json/(?P<json>\w+)/(?P<tri>\w+)', 'json'),
+    url(r'^gallerie/(?P<nom_gallerie>\w+)', 'gallerie'),
     url(r'^$', 'accueil'),
 )
