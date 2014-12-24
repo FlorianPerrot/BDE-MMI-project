458599f1b13bb68dce6af0e62ea2c3c15b1163c2
Florian Perrot <contact@florian-perrot.fr>
2014-12-24 23:20:12 +0100

    nav : ajout du titre + nom de la gallerie

--- bdeproject/urls.py
+++ bdeproject/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include('vitrine.urls')),
+    url(r'^', include('vitrine.urls', namespace="vitrine")),
     url(r'^admin/', include(admin.site.urls)),
 )
 
